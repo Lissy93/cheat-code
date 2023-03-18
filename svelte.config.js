b08dceb922b3c62786d6bca95ac875bb4346ebc4
Alicia Sykes <alicia@omg.lol>
2023-03-18 18:35:54 +0000

    Adds Netlify and Vercel deploy adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,34 @@
-import adapter from '@sveltejs/adapter-auto';
+import autoAdapter from '@sveltejs/adapter-auto';
+import netlifyAdapter from '@sveltejs/adapter-netlify';
+import vercelAdapter from '@sveltejs/adapter-vercel';
+import nodeAdapter from '@sveltejs/adapter-node';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const multiAdapter = (adapters) => {
+  return {
+    async adapt(argument) {
+      await Promise.all(
+        adapters.map((item) =>
+          Promise.resolve(item).then((resolved) => resolved.adapt(argument))
+        )
+      );
+    },
+  };
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter(),
+    adapter: multiAdapter([
+      autoAdapter(),
+      netlifyAdapter(),
+      vercelAdapter(),
+      nodeAdapter(),
+    ]),
     alias: {
-      '$/*': 'src/*',
+      '$src/*': 'src/*',
     },
   },
 };
